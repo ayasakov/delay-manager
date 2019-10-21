ae6b2a11e4bb684e509d149ff5e0def363b3b0af
Artem Yasakov <jasakovartem@gmail.com>
2019-10-21 13:18:06 +0400

    fix: change client id for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   tokenRedirectUri: 'http://work-delay.herokuapp.com/auth/login',
-  slackClientId: '3800862307.723008059171',
+  slackClientId: '3800862307.791069167635',
   serverHost: 'http://work-delay.herokuapp.com',
 };
